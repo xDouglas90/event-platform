67937bfdd3b26d93d57df1034715222bc1e1c2fd
xDouglas90 <xdouglas90@gmail.com>
2022-06-22 08:17:51 -0300

    chore: create page file

---
+++ src/pages/Event.tsx
@@ -0,0 +1,13 @@
+import { Header, Sidebar, Video } from '../components';
+
+export const Event = () => {
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='flex flex-1'>
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
+  );
+};
