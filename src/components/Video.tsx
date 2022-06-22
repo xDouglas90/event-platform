25415675a2f13f0072d47b70fc541bfcd2870aef
xDouglas90 <xdouglas90@gmail.com>
2022-06-22 08:18:51 -0300

    feat: create components and get data from api

---
+++ src/components/Video.tsx
@@ -0,0 +1,8 @@
+export const Video = () => {
+    return (
+      <section className="flex-1">
+        <h1>Video</h1>
+      </section>
+    );
+  };
+  
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { Header } from './Header';
+import { Lesson } from './Lesson';
+import { Logo } from './Logo';
+import { Sidebar } from './Sidebar';
+import { Video } from './Video';
+
+export { Header, Lesson, Logo, Sidebar, Video };
